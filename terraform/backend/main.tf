d62ec3dfa92c12cd7784423ed52949e09c3b73d8
Laura Edith Mata <73843582+lauraehm@users.noreply.github.com>
2021-03-11 14:04:18 -0600

    Update main.tf

--- terraform/backend/main.tf
+++ terraform/backend/main.tf
@@ -12,10 +12,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "gh-test" {
-  bucket = "gh-test-7"
+  bucket = "gh-test-8"
 }
 
-resource "aws_s3_bucket" "s3" {
+resource "aws_s3_bucket" "s3-test" {
   bucket = "s3-terraform-backend"
 
   lifecycle {
@@ -35,7 +35,7 @@ resource "aws_s3_bucket" "s3" {
   }
 }
 
-resource "aws_dynamodb_table" "dynamo" {
+resource "aws_dynamodb_table" "dynamo-test" {
   name         = "dynamo-terraform-backend"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
