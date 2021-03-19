9d7c6f7bbe2f568b582b0e5784358d2b2134ed73
lauraehm <laura.mata@wizeline.com>
2021-03-18 18:07:23 -0600

    ÂPath was wrong, now is not

--- lambda-test/lambda_function.py
+++ lambda-test/lambda_function.py
@@ -4,4 +4,4 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda and Terraform :D!')
+        'body': json.dumps('Hello from Lambda and Terraform please!')
