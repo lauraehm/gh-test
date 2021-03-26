a54e01cb31ee5272fb218ccbc4c0d47d258cbed0
lauraehm <laura.mata@wizeline.com>
2021-03-25 18:42:28 -0600

    J

--- lambda-test/lambda_function.py
+++ lambda-test/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('This is an interesting test :PP!')
+        'body': json.dumps('This is an interesting test :PPP!')
     }
