99069a58ef113ba9fbe11402715707cb294ab230
lauraehm <laura.mata@wizeline.com>
2021-03-26 10:58:59 -0600

    Z6

--- lambda-test/lambda_function.py
+++ lambda-test/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('HALP PLEASE DDDD:')
+        'body': json.dumps('HALP PLEASE :sad:')
     }
