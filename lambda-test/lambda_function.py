ce6f3ab3e207392b7f4a660440f9d91017940663
lauraehm <laura.mata@wizeline.com>
2021-03-26 14:04:02 -0600

    one last test before I give up 2

--- lambda-test/lambda_function.py
+++ lambda-test/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('HALP PLEASE :cc')
+        'body': json.dumps('HALP PLEASE :ccc')
     }
